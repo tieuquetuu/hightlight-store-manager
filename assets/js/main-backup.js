a4dd57b540bd70f7e962d2f9df5a57faaa405594
trung hieu <hieusmall1102@gmail.com>
2022-07-25 01:35:11 +0700

    update javascript

---
+++ assets/js/main-backup.js
@@ -0,0 +1,376 @@
+let $, 
+	HightLightStoreClient;
+
+if (typeof jQuery !== 'undefined') {
+	$ = jQuery;
+}
+
+if (typeof hightlight_client_object !== 'undefined') {
+	HightLightStoreClient = hightlight_client_object;
+}
+
+function is_hl_product_page() {
+	return HightLightStoreClient?.is_hightlight_product === '1';
+}
+
+/**
+ * @name: 
+ * @description: Gửi Thông tin trang mỗi lần load page
+ * 
+ */
+
+/*function TrackingPageView() {
+	gtag('event', 'page_view', {
+	  	// 'event_category': "",
+	  	'event_label': "Xem trang",
+	  	...HightLightStoreClient
+	});
+}
+
+
+function TrackingViewProduct() {
+	
+	if (is_hl_product_page()) {
+		console.log("Đây là trang sản phẩm")
+
+		gtag("event", "view_product_item", {
+		  	"Tên miền": HightLightStoreClient?.hostname,
+		  	"Đường dẫn": location.pathname,
+		  	"Đường dẫn chi tiết": location.href,
+		  	'event_label': "Xem trang sản phẩm",
+		  	...HightLightStoreClient
+		})
+
+	};
+
+	/!**!/
+}
+
+
+function TrackingClickGoToStore() {
+	$('.hl-cua-hang').on('click', function(event){
+		console.log("User click vào button cửa hàng")
+		gtag("event", "click_view_shop", {
+		  	"Tên miền": location.hostname,
+		  	"Đường dẫn": location.pathname,
+		  	"Đường dẫn chi tiết": location.href,
+		  	'event_label': "Nhấn nút cửa hàng",
+		  	...HightLightStoreClient
+		});
+	})
+}
+
+
+function TrackingClickBuyProduct() {
+	$('.hl-mua-hang').on('click', function(event){
+		console.log("User click vào button mua hàng")
+		gtag("event", "click_buy_product", {
+		  	"Tên miền": location.hostname,
+		  	"Đường dẫn": location.pathname,
+		  	"Đường dẫn chi tiết": location.href,
+		  	'event_label': "Nhấn nút mua hàng",
+		  	...HightLightStoreClient
+		});
+	})
+}
+
+
+function SetupTracking() {
+	TrackingClickGoToStore()
+	TrackingClickBuyProduct()
+	TrackingViewProduct()
+	TrackingPageView()
+}*/
+
+function SetupDataTable() {
+ 	let $tables = $(".store-hightlight-dataTable");
+	let dataTableOptions = {
+		// rowGroup: true,
+		responsive: true,
+		// dom: 'Bfrtip',
+		/*buttons: [
+			'excel'
+		]*/
+	}
+	if ($tables.length) {
+		$tables.each(function (tableIndex,tableDom) {
+			let $table = $(tableDom);
+			let $dataTable = $table.DataTable(dataTableOptions);
+
+			if ($table.hasClass("admin-view")) {
+				$table.find("tbody").on("click", "tr", function(e) {
+					// $(this).toggleClass('selected')
+					let tr = this;
+					let $tr = $(tr);
+					let rowData = $dataTable.row(tr).data();
+
+					$("#detail-analytics-modal").addClass("show");
+
+					console.log($tr.data())
+				});
+			}
+		})
+	}
+
+	// if ($(".store-hightlight-dataTable").length) {
+	// 	let dataTableOptions = {
+	// 		// rowGroup: true,
+	// 		responsive: true,
+	// 		// dom: 'Bfrtip',
+	// 		buttons: [
+	// 			'copy', 'excel', 'pdf'
+	// 		]
+	// 	}
+	// 	$(".store-hightlight-dataTable").DataTable(dataTableOptions);
+	// }
+}
+
+function setupManagementDataTable() {
+	let $tables = $(".store-hightlight-management-dataTable");
+	let dataTableOptions = {
+		responsive: true,
+		processing: true,
+		serverSide: true,
+	}
+
+	if ($tables.length) {
+		$tables.each(function (tableIndex,tableDom) {
+			let $table = $(tableDom);
+			let { ajaxSource } = $table.data();
+			if (ajaxSource) {
+				dataTableOptions.ajax = ajaxSource;
+			}
+			let $dataTable = $table.DataTable(dataTableOptions);
+		})
+	}
+}
+
+function systemManagementTable() {
+	let $table = $("#system-management-dataTable");
+	let { ajaxSource } = $table.data();
+
+	$table.DataTable({
+		processing: true,
+		serverSide: true,
+		ajax: ajaxSource,
+		columns: [
+			{
+				data: "id",
+			},
+			{
+				data: "title",
+			},
+			{
+				data: "author.display_name",
+			},
+			{
+				render: (index, text, row) => {
+					let tongLuotXem = 0
+					for (let i = 0; i < row.analytics_data.length;i++) {
+						tongLuotXem += parseInt(row.analytics_data[i].screenPageViews);
+					}
+					return tongLuotXem
+				}
+			},
+			{
+				render: (index, text, row) => {
+					let tongClickCuaHang = 0
+					for (let i = 0; i < row.analytics_data.length;i++) {
+						if (row.analytics_data[i].eventName === "click_buy_product") {
+							tongClickCuaHang += parseInt(row.analytics_data[i].eventCount);
+						}
+					}
+					return tongClickCuaHang
+				}
+			},
+			{
+				render: (index, text, row) => {
+					let tongClickMuaHang = 0
+					for (let i = 0; i < row.analytics_data.length;i++) {
+						if (row.analytics_data[i].eventName === "click_view_shop") {
+							tongClickMuaHang += parseInt(row.analytics_data[i].eventCount);
+						}
+					}
+					return tongClickMuaHang
+				}
+			},
+			{
+				render: (index, text, row) => {
+					let tongThoiGianTrungBinh = 0
+					for (let i = 0; i < row.analytics_data.length;i++) {
+						let count = parseFloat(row.analytics_data[i].averageSessionDuration);
+
+						tongThoiGianTrungBinh += count;
+					}
+
+					if (row.analytics_data.length > 0) {
+						tongThoiGianTrungBinh = tongThoiGianTrungBinh / row.analytics_data.length;
+					}
+
+					return tongThoiGianTrungBinh
+				}
+			},
+			{
+				data: "status",
+			}
+		]
+	});
+}
+
+/**
+ * SET UP MODAL JAVASCRIPT
+ */
+
+function setupModal() {
+	let openModalBtn = $("#myBtn");
+	let detailAnalyticsModal = $("#detail-analytics-modal");
+	let closeBtn = detailAnalyticsModal.find(".close");
+
+	openModalBtn.on("click", function(e) {
+		detailAnalyticsModal.toggleClass("show");
+	})
+
+	closeBtn.on("click", function (e) {
+		detailAnalyticsModal.removeClass("show");
+	})
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == detailAnalyticsModal[0]) {
+			detailAnalyticsModal.removeClass("show");
+		}
+	}
+}
+
+//Multi CheckBox
+var defaultMultiCheckBoxOption = { width: '220px', defaultText: 'Select Below', height: '200px' };
+
+	jQuery.fn.extend({
+		CreateMultiCheckBox: function (options) {
+
+			var localOption = {};
+			localOption.width = (options != null && options.width != null && options.width != undefined) ? options.width : defaultMultiCheckBoxOption.width;
+			localOption.defaultText = (options != null && options.defaultText != null && options.defaultText != undefined) ? options.defaultText : defaultMultiCheckBoxOption.defaultText;
+			localOption.height = (options != null && options.height != null && options.height != undefined) ? options.height : defaultMultiCheckBoxOption.height;
+
+			this.hide();
+			this.attr("multiple", "multiple");
+			var divSel = $("<div class='MultiCheckBox'>" + localOption.defaultText + "<span class='k-icon k-i-arrow-60-down'><svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='sort-down' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512' class='svg-inline--fa fa-sort-down fa-w-10 fa-2x'><path fill='currentColor' d='M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z' class=''></path></svg></span></div>").insertBefore(this);
+			divSel.css({ "width": localOption.width });
+
+			var detail = $("<div class='MultiCheckBoxDetail'><div class='MultiCheckBoxDetailHeader'><input type='checkbox' class='mulinput' value='-1982' /><div>Select All</div></div><div class='MultiCheckBoxDetailBody'></div></div>").insertAfter(divSel);
+			detail.css({ "width": parseInt(options.width) + 10, "max-height": localOption.height });
+			var multiCheckBoxDetailBody = detail.find(".MultiCheckBoxDetailBody");
+
+			this.find("option").each(function () {
+				var val = $(this).attr("value");
+
+				if (val == undefined)
+					val = '';
+
+				multiCheckBoxDetailBody.append("<div class='cont'><div><input type='checkbox' class='mulinput' value='" + val + "' /></div><div>" + $(this).text() + "</div></div>");
+			});
+
+			multiCheckBoxDetailBody.css("max-height", (parseInt($(".MultiCheckBoxDetail").css("max-height")) - 28) + "px");
+		},
+		UpdateSelect: function () {
+			var arr = [];
+
+			this.prev().find(".mulinput:checked").each(function () {
+				arr.push($(this).val());
+			});
+
+			this.val(arr);
+		},
+	});
+function setupMultiCheckBox() {
+	
+
+	$(document).on("click", ".MultiCheckBox", function () {
+		var detail = $(this).next();
+		detail.show();
+	});
+
+	$(document).on("click", ".MultiCheckBoxDetailHeader input", function (e) {
+		e.stopPropagation();
+		var hc = $(this).prop("checked");
+		$(this).closest(".MultiCheckBoxDetail").find(".MultiCheckBoxDetailBody input").prop("checked", hc);
+		$(this).closest(".MultiCheckBoxDetail").next().UpdateSelect();
+	});
+
+	$(document).on("click", ".MultiCheckBoxDetailHeader", function (e) {
+		var inp = $(this).find("input");
+		var chk = inp.prop("checked");
+		inp.prop("checked", !chk);
+		$(this).closest(".MultiCheckBoxDetail").find(".MultiCheckBoxDetailBody input").prop("checked", !chk);
+		$(this).closest(".MultiCheckBoxDetail").next().UpdateSelect();
+	});
+
+	$(document).on("click", ".MultiCheckBoxDetail .cont input", function (e) {
+		e.stopPropagation();
+		$(this).closest(".MultiCheckBoxDetail").next().UpdateSelect();
+
+		var val = ($(".MultiCheckBoxDetailBody input:checked").length == $(".MultiCheckBoxDetailBody input").length)
+		$(".MultiCheckBoxDetailHeader input").prop("checked", val);
+	});
+
+	$(document).on("click", ".MultiCheckBoxDetail .cont", function (e) {
+		var inp = $(this).find("input");
+		var chk = inp.prop("checked");
+		inp.prop("checked", !chk);
+
+		var multiCheckBoxDetail = $(this).closest(".MultiCheckBoxDetail");
+		var multiCheckBoxDetailBody = $(this).closest(".MultiCheckBoxDetailBody");
+		multiCheckBoxDetail.next().UpdateSelect();
+
+		var val = ($(".MultiCheckBoxDetailBody input:checked").length == $(".MultiCheckBoxDetailBody input").length)
+		$(".MultiCheckBoxDetailHeader input").prop("checked", val);
+	});
+
+	$(document).mouseup(function (e) {
+		var container = $(".MultiCheckBoxDetail");
+		if (!container.is(e.target) && container.has(e.target).length === 0) {
+			container.hide();
+		}
+	});
+
+	$(".MultiCheckBox.MultiCheckBox-LoaiSanPham").CreateMultiCheckBox({ width: '230px', defaultText : 'Loại Sản Phẩm', height:'250px' });
+	$(".MultiCheckBox.MultiCheckBox-LoaiDichVu").CreateMultiCheckBox({ width: '230px', defaultText : 'Loại Dịch Vụ', height:'250px' });
+}
+/**
+	 * Login
+	 */
+ $('.form-login-store-hightlight-manager').on('submit', function(e) {
+	e.preventDefault();
+	$form = $(this);
+	$form.find('[type="submit"]').append('<i class="ion-loop spin icon icon-right"></i>');
+	$form.find('.alert').remove();
+	$.ajax({
+		type: 'POST',
+		url: ajax.ajax_url,
+		data: $form.serialize(),
+		success: function( data, textStatus, jqXHR ) {
+			$form.find('[type="submit"]').find('.icon').remove();
+			if( data.status == true ) {
+				$form.append('<div class="alert alert-success">' + data.message + '</div>');
+				$form.find('.form-control').val('');
+				if( $form.hasClass('form-login') ) {
+					window.location.href = '/quan-ly-du-lieu-san-pham/admin';
+				}
+			} else {
+				$form.append('<div class="alert alert-danger">' + data.message + '</div>');
+			}
+		},
+		error: function( jqXHR, textStatus, errorThrown ) {
+			alert( errorThrown );
+		}
+	});
+});
+// Setup Data Table
+$(document).ready(function() {
+	SetupDataTable();
+	// setupManagementDataTable();
+	// systemManagementTable();
+	setupModal();
+	setupMultiCheckBox();
+})
---
+++ assets/js/system-manager.js
@@ -0,0 +1,283 @@
+function SystemManagerInit() {
+    let $table = $("#system-report-table");
+    let { ajaxSource } = $table.data();
+    if (!ajaxSource) {
+        return false;
+    }
+
+    let $dataTable = $table.DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: ajaxSource,
+        /*ajax: {
+            url: ajaxSource,
+            contentType: "application/json",
+            type: "POST",
+            data: function(data, settings){
+                return JSON.stringify( data );
+            }
+        },*/
+        columns: [
+            {
+                className:      'dt-control',
+                orderable:      false,
+                data:           null,
+                defaultContent: '',
+            },
+            {
+                className:      'details-control-id',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    return data?.id
+                }
+            },
+            {
+                className:      'details-control-title',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    return data?.title
+                }
+            },
+            {
+                className:      'details-control-category',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    let categoryName = data?.category.map(obj => obj.name);
+
+                    categoryName = categoryName.join(",");
+
+                    return categoryName
+                }
+            },
+            {
+                className:      'details-control-author',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    return data?.author?.display_name
+                }
+            },
+            {
+                className:      'text-center details-control-luot-xem',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    let totalScreenPageViews = 0;
+
+                    let { analytics } = data;
+
+                    if (!analytics || analytics.length <= 0) {
+                        return totalScreenPageViews
+                    }
+
+                    for (let i = 0;i < analytics.length;i++) {
+                        let analyticsItem = analytics[i];
+                        totalScreenPageViews += parseInt(analyticsItem?.screenPageViews);
+                    }
+
+                    return `${totalScreenPageViews} lượt xem`;
+                }
+            },
+            {
+                className:      'text-center details-control-luot-click-cua-hang',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    let totalClick = 0;
+
+                    let { analytics } = data;
+
+                    let analytics_click_view_shop = analytics.filter(obj => obj.eventName === "click_view_shop");
+
+                    if (!analytics || analytics.length <= 0 || !analytics_click_view_shop) {
+                        return totalClick
+                    }
+
+                    for (let i = 0;i < analytics_click_view_shop.length;i++) {
+                        let analyticsItem = analytics_click_view_shop[i];
+                        totalClick += parseInt(analyticsItem?.eventCount);
+                    }
+
+                    return `${totalClick} lượt`
+                }
+            },
+            {
+                className:      'text-center details-control-luot-click-mua-hang',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    let totalClick = 0;
+
+                    let { analytics } = data;
+
+                    let analytics_click_buy_product = analytics.filter(obj => obj.eventName === "click_buy_product");
+
+                    if (!analytics || analytics.length <= 0 || !analytics_click_buy_product) {
+                        return totalClick
+                    }
+
+                    for (let i = 0;i < analytics_click_buy_product.length;i++) {
+                        let analyticsItem = analytics_click_buy_product[i];
+                        totalClick += parseInt(analyticsItem?.eventCount);
+                    }
+
+                    return `${totalClick} lượt`
+                }
+            },
+            {
+                className:      'text-right details-control-thoi-gian-xem-trung-binh',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    let averageSessionDuration = 0;
+
+                    let { analytics } = data;
+                    if (!analytics || analytics.length <= 0) {
+                        return averageSessionDuration
+                    }
+
+                    for (let i = 0;i < analytics.length;i++) {
+                        let analyticsItem = analytics[i];
+                        averageSessionDuration += parseFloat(analyticsItem?.averageSessionDuration);
+                    }
+
+                    averageSessionDuration = averageSessionDuration / analytics.length;
+
+                    return `${averageSessionDuration} giây`
+                }
+            },
+            {
+                className:      'details-control-status',
+                orderable:      false,
+                data:           null,
+                defaultContent: 'không có dữ liệu',
+                render: (row, type, data) => {
+                    return data?.status
+                }
+            },
+        ],
+        dom: 'Bfrtip',
+        buttons: [
+            'excel', 'pdf'
+        ]
+    });
+
+    $table.on('click', 'td.dt-control', function (e) {
+        let $tr = $(this).closest('tr');
+        let row = $dataTable.row($tr);
+        let rowData = row.data()
+        let { analytics } = rowData;
+
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            $tr.removeClass('shown');
+        } else {
+            // Open this row
+            row.child(analyticsFormat(analytics)).show();
+            // console.log(analyticsFormat(analytics))
+            $tr.addClass('shown');
+        }
+    });
+
+    function analyticsFormat(dataAnalytics) {
+        // `d` is the original data object for the row
+        let rows = '';
+
+        if (dataAnalytics.length == 0) {
+            rows = `
+               <tr colspan="5">
+                    <td class="text-center" colspan="5">Không có dữ liệu</td>
+                </tr>
+            `;
+        } else {
+
+            let data = {}
+
+            let dataByHostNames = {};
+
+            dataAnalytics.forEach(function(obj, count){
+                let { eventName, eventCount, screenPageViews, hostName,averageSessionDuration  } = obj;
+                let hostNameKey = hostName;
+
+                if (!(hostNameKey in dataByHostNames)) {
+                    dataByHostNames[hostNameKey] = {
+                        "hostName": hostName,
+                        "click_view_shop" : 0,
+                        "click_buy_product" : 0,
+                        "screenPageViews" : 0,
+                        "averageSessionDuration" : 0,
+                    };
+                }
+
+                if (eventName === "click_buy_product") {
+                    dataByHostNames[hostNameKey]["click_buy_product"] += parseInt(eventCount);
+                }
+                if (eventName === "click_view_shop") {
+                    dataByHostNames[hostNameKey]["click_view_shop"] += parseInt(eventCount);
+                }
+                dataByHostNames[hostNameKey]["screenPageViews"] += parseInt(screenPageViews);
+                dataByHostNames[hostNameKey]["averageSessionDuration"] += parseFloat(averageSessionDuration);
+            })
+
+            // Map lại thời gian xem trung bình
+
+
+            Object.values(dataByHostNames).forEach(function(d) {
+
+                let totalItems = dataAnalytics.filter(obj => obj.hostName === d.hostName).length;
+
+                 rows += `
+                    <tr>
+                        <td>${d.hostName}</td>
+                        <td class="text-center">${d.screenPageViews} lượt xem</td>
+                        <td class="text-center">${d.click_view_shop} lượt</td>
+                        <td class="text-center">${d.click_buy_product} lượt</td>
+                        <td class="text-right">${d.averageSessionDuration / totalItems} giây</td>
+                    </tr>
+                 `
+            });
+        }
+
+        return (
+            `<table class="system-report-table-child-row-details display" style="width: 100%">
+                <thead>
+                    <tr>
+                        <th>Tên miền</th>
+                        <th class="text-center">Lượt xem</th>
+                        <th class="text-center">Lượt click cửa hàng</th>
+                        <th class="text-center">Lượt click mua hàng</th>
+                        <th class="text-right">Thời gian xem trung bình</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${rows}       
+                </tbody>
+            </table>`
+        );
+    }
+
+    if ($("#filter-user").length) {
+        $("#filter-user").on("change", function (e) {
+            let value = e.currentTarget.value;
+
+        })
+    }
+}
+
+if (typeof $ != undefined) {
+    $(document).ready(function() {
+        SystemManagerInit();
+    })
+}
